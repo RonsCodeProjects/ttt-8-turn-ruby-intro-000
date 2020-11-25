2a928e26c55da240be48e7a48193f4de9886d9c2
Ronald Colonna <roncolonna@yahoo.com>
2020-11-25 22:51:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,10 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, player)
+   board[index] = player
+end
+
 def player_move(board, index, marker)
  board[index] = marker
 end
