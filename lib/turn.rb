e7e02578c2e048d54f299b42984fe5d860eeded4
Ronald Colonna <roncolonna@yahoo.com>
2020-11-25 22:54:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,7 +43,7 @@ def turn(board)
   
   if valid_move?(board, index)
 #    player_move(board, index, current_player(board))
-    move(board,index,letter)
+    move(board,index,"x")
 
     display_board(board)
   else
