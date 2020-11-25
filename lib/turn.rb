62ac677da5325da16eb903b9a1b028d58b2024da
Ronald Colonna <roncolonna@yahoo.com>
2020-11-25 22:43:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,13 @@ def player_move(board, index, marker)
  board[index] = marker
 end
 
-
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
 
 def valid_move?(board, index)
   if !position_taken?(board, index) && (index).between?(0,8)
